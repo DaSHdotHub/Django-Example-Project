13d6f6afdd3cbf12807474300ac184aa0a866212
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-16 06:19:51 +0100

    Add model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Publish"))
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
