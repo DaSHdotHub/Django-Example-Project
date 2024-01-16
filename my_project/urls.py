9f1e1f35c4997609a75f9466ce131f5bb4d5dadf
dshaulov1 <dshaulov@cse-healthcare.io>
2024-01-16 17:24:11 +0100

    Add html template

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import home
 urlpatterns = [
-    path('', home),
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
